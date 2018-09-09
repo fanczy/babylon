f1334e627c34e11bc9f156d96abca0644482440b
fanczy <fanczy@gmail.com>
2018-09-09 21:36:36 +0200

    initial

--- lab/main.js
+++ lab/main.js
@@ -1,5 +1,17 @@
 import { mainCanvasEngine, mainScene } from "./src/lab.js";
 
+let frame = 0;
+const step = 10 ** -2;
+let currentStep = 0;
+
 mainCanvasEngine.runRenderLoop(() => {
+    frame += 1
+    currentStep = frame * step;
+
+    
+
+    mainScene.getMeshByName("sphere2").position.x += currentStep / 10;
+    mainScene.getMeshByName("sphere2").position.y += (Math.sin(currentStep));
+
     mainScene.render();
 });
--- lab/src/lab.js
+++ lab/src/lab.js
@@ -3,13 +3,14 @@ export const mainCanvasEngine = new BABYLON.Engine(mainCanvas, true);
 export const mainScene = new BABYLON.Scene(mainCanvasEngine);
 mainScene.clearColor = new BABYLON.Color3.Black();
 
-export const mainCamera = new BABYLON.UniversalCamera(
-    "mainCamera",
-    new BABYLON.Vector3(0,0,0),
-    mainScene
-)
-mainCamera.setTarget(BABYLON.Vector3.Zero());
-mainCamera.attachControl(mainCanvas, true);
+// export const mainCamera = new BABYLON.UniversalCamera(
+//     "mainCamera",
+//     new BABYLON.Vector3(0,0,0),
+//     mainScene
+// )
+
+// mainCamera.setTarget(BABYLON.Vector3.Zero());
+// mainCamera.attachControl(mainCanvas, true);
 
 export const wireFrameMaterial = new BABYLON.StandardMaterial("wireframe", mainScene);
 wireFrameMaterial.wireframe = true;
@@ -27,13 +28,29 @@ const setMeshPositionAndMaterial = (mesh, positionVector, material) => {
 const addBox = (id, size, positionVector, material) => {
     const box = BABYLON.MeshBuilder.CreateBox(id, {size: size || 100} , mainScene);
     setMeshPositionAndMaterial(box, positionVector, material);
+    return box;
 }
 
 const addSphere = (id, diameter,  positionVector, material) => {
     const sphere = BABYLON.MeshBuilder.CreateSphere(id, {diameter: diameter || 100}, mainScene);
     setMeshPositionAndMaterial(sphere, positionVector, material);
+    return sphere;
 }
 
-addBox("box1", 50 , null, blueWireFrameMaterial);
-addSphere("sphere", 500);
-addSphere("sphere2", 10, BABYLON.Vector3(200,100,100));
+const box = addBox("box1", 50 , null, blueWireFrameMaterial);
+const sphere1 = addSphere("sphere1", 500);
+const sphere2 = addSphere("sphere2", 10, BABYLON.Vector3(200,100,100));
+
+
+export const followCamera = new BABYLON.FollowCamera(
+    "followCamera",
+    new BABYLON.Vector3(-1000,1000, 0),
+    mainScene
+)
+
+followCamera.radius = 100;
+followCamera.heightOffset = 0;
+followCamera.rotationOffset = 0;
+followCamera.cameraAcceleration = 0.05;
+followCamera.attachControl(mainCanvas, true);
+followCamera.lockedTarget = sphere2;
